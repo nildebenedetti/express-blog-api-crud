0ebb241f31322131a841b427cb116e7de06efb13
“Nilde <nilde.benedetti@gmail.com>
2026-05-26 15:56:53 +0200

    fix: rotta che era rotta (...)

--- routes/posts.js
+++ routes/posts.js
@@ -17,7 +17,7 @@ router.get('/posts/:id', show);
 
 // create - POST http://localhost:3000
 
-router.post('posts/', create)
+router.post('/posts/', create)
 
 // update - PUT || PATCH http://localhost:3000/posts/:id
 
