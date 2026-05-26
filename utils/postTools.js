74d5ba0f529ab24b7df8be9bb841332d6d7deeda
“Nilde <nilde.benedetti@gmail.com>
2026-05-26 16:54:33 +0200

    add: validazione dei duplicati nella funzione di creazione slug

--- utils/postTools.js
+++ utils/postTools.js
@@ -23,5 +23,26 @@ const craftSlug = (post) => {
     .replace(/[^a-zA-Z0-9\s]/g, '') // buongiorno RegEx potentissimaaa
     .replaceAll(" ", "-");
 
-    return slug;
+    // per evitare la creazione di duplicati devo 
+    // crearmi un increment a 0 e poi impostare un do while
+
+    let increment = 0;
+    let finalSlug;
+    let postwithSameSlug;
+
+    do {
+
+        // qui mi metto lo slug che viene aggiornato ad ogni giro di check
+        finalSlug = slug + ((increment === 0) ? '' : `-${increment}`)
+
+        // CERCA SE c`è uno slug uguale a questo,
+            // SE C'É increment++
+        postwithSameSlug = posts.find(post => {
+            return post.slug === finalSlug;
+        });
+
+        increment++;
+    } while (postwithSameSlug !== undefined); // FALLO FINO A QUANDO non ce ne sono più
+
+    return finalSlug; // RESTITUISCI LO SLUG + "-increment"
 }
