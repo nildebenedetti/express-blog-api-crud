8670b9ac31956223cd0c8f04b5f94db5ee78da62
“Nilde <nilde.benedetti@gmail.com>
2026-05-27 18:10:39 +0200

    fix: rimosso errore di test da router

--- routes/posts.js
+++ routes/posts.js
@@ -13,11 +13,6 @@ import validateTags from '../middlewares/validateTags.js';
 
 const router = express.Router();
 
-router.get('/test-error', (req, res) => {
-    // Lanciamo un errore intenzionale
-    throw new Error("Mamma mia! Il test dell'errorHandler è riuscito!");
-});
-
 // index - GET  http://localhost:3000
 
 router.get('/', index);
