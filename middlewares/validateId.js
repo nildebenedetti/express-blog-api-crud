7fb4c394ccc1a1f3fec7f9e73db4a94ac975406b
“Nilde <nilde.benedetti@gmail.com>
2026-05-27 16:02:46 +0200

    fix: naming funzione validateId

---
+++ middlewares/validateId.js
@@ -0,0 +1,23 @@
+
+function validateId(request, response, next) {
+    const { id } = request.params;
+    const realId = Number(id.trim()); // normalizzo id a numero
+    if (isNaN(realId)) {
+        response.status(400)
+            .json({
+                error: 'Id non corretto: inserire un numero valido!'
+            })
+        return;
+    } else if (realId <= 0) {
+        response.status(400)
+            .json({
+                error: 'ripigliati... hai inserito un valore negativo'
+            })
+        return;
+    }
+    request.realId = realId;
+    next();
+
+}
+
+export default validateId;
